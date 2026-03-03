8c9b7238ca1f7bbe562d8a1e66d79c0dec574f7f
Enzo Galvani <enzobgalvani@gmail.com>
2026-03-03 18:17:58 -0300

    Add files via upload

---
+++ Exercicios/Exercicio 1/script.js
@@ -0,0 +1,25 @@
+//Exercicio 1
+let nome = "Enzo"
+let idade = 16
+let curso = "Infonet"
+console.log("Meu nome é " + nome + " tenho "+ idade + " anos, e faço o curso " + curso)
+
+//Exercicio 2
+let num1=6
+let num2=7
+let soma1=num1+num2
+console.log(soma1)
+
+//Exercicio 3
+let num3=10
+let num4=2
+console.log(num3+num4)
+console.log(num3-num4)
+console.log(num3*num4)
+console.log(num3/num4)
+console.log(num3%num4)
+
+//Exercicio 4
+let num5="30"
+let numeroInteiro = Number(num5)
+console.log(numeroInteiro+20)
